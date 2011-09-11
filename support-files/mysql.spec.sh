779adc3737883e6f0c75cc6c14164d3b1b9ced55
Alex Yurchenko <ayurchen@void>
2011-09-11 11:09:57 +0300

    References lp:844356 adding README-wsrep to 5.5 RPM spec

--- support-files/mysql.spec.sh
+++ support-files/mysql.spec.sh
@@ -995,6 +995,7 @@ echo "====="                                     >> $STATUS_HISTORY
 %doc release/Docs/INFO_BIN*
 %doc release/support-files/my-*.cnf
 %if %{defined with_wsrep}
+%doc release/Docs/README-wsrep
 %doc release/support-files/wsrep.cnf
 %endif
 
