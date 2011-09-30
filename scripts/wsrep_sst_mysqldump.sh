0600afd1b08b2643ba71729e830afd247904bf12
Alex Yurchenko <ayurchen@void>
2011-09-30 03:43:09 +0300

    References lp:862184 - SQL comments in the CSV tables fix script seem to break things. Removed.

--- scripts/wsrep_sst_mysqldump.sh
+++ scripts/wsrep_sst_mysqldump.sh
@@ -59,23 +59,17 @@ MYSQLDUMP="mysqldump $AUTH -h$LOCAL_HOST -P$LOCAL_PORT \
 --skip-comments --flush-privileges --all-databases"
 
 # mysqldump cannot restore CSV tables, fix this issue
-CSV_FIX="
---
--- Fix the system tables of MySQL Server
---
-
+CSV_TABLES_FIX="
 set sql_mode='';
 
 USE mysql;
 
--- Create general_log if CSV is enabled.
 SET @str = IF (@@have_csv = 'YES', 'CREATE TABLE IF NOT EXISTS general_log (event_time TIMESTAMP NOT NULL, user_host MEDIUMTEXT NOT NULL, thread_id INTEGER NOT NULL, server_id INTEGER UNSIGNED NOT NULL, command_type VARCHAR(64) NOT NULL,argument MEDIUMTEXT NOT NULL) engine=CSV CHARACTER SET utf8 comment=\"General log\"', 'SET @dummy = 0');
 
 PREPARE stmt FROM @str;
 EXECUTE stmt;
 DROP PREPARE stmt;
 
--- Create slow_log if CSV is enabled.
 SET @str = IF (@@have_csv = 'YES', 'CREATE TABLE IF NOT EXISTS slow_log (start_time TIMESTAMP NOT NULL, user_host MEDIUMTEXT NOT NULL, query_time TIME NOT NULL, lock_time TIME NOT NULL, rows_sent INTEGER NOT NULL, rows_examined INTEGER NOT NULL, db VARCHAR(512) NOT NULL, last_insert_id INTEGER NOT NULL, insert_id INTEGER NOT NULL, server_id INTEGER UNSIGNED NOT NULL, sql_text MEDIUMTEXT NOT NULL) engine=CSV CHARACTER SET utf8 comment=\"Slow log\"', 'SET @dummy = 0');
 
 PREPARE stmt FROM @str;
@@ -83,9 +77,6 @@ EXECUTE stmt;
 DROP PREPARE stmt;"
 
 SET_START_POSITION="SET GLOBAL wsrep_start_position='$UUID:$SEQNO';"
-
-# Error message to cause mysql process to abort in case of mysqldump error
-MYSQLDUMP_ERROR="'$MYSQLDUMP' failed with $?"
 
 #MYSQL="@bindir@/mysql -u'$USER' -p'$PSWD' -h'$HOST' -P'$PORT'"
 MYSQL="mysql $AUTH -h$HOST -P$PORT --disable-reconnect --connect_timeout=10"
@@ -102,6 +93,8 @@ $MYSQL -e"$STOP_WSREP SET GLOBAL SLOW_QUERY_LOG=OFF"
 RESTORE_GENERAL_LOG="SET GLOBAL GENERAL_LOG=$GENERAL_LOG_OPT;"
 RESTORE_SLOW_QUERY_LOG="SET GLOBAL SLOW_QUERY_LOG=$SLOW_LOG_OPT;"
 
-((echo $STOP_WSREP && $MYSQLDUMP && echo $CSV_FIX && echo $RESTORE_GENERAL_LOG && echo $RESTORE_SLOW_QUERY_LOG && echo $SET_START_POSITION) || echo $MYSQLDUMP_ERROR) | $MYSQL
+(echo $STOP_WSREP && $MYSQLDUMP && echo $CSV_TABLES_FIX \
+&& echo $RESTORE_GENERAL_LOG && echo $RESTORE_SLOW_QUERY_LOG \
+&& echo $SET_START_POSITION) | $MYSQL
 
 #
