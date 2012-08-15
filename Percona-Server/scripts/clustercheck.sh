a48cf9c91480236a2783790d33b0062aed903d86
vadim@percona.com <>
2012-08-14 21:43:17 -0700

    Included fixed clustercheck.sh script from Olaf van Zandwijk

--- Percona-Server/scripts/clustercheck.sh
+++ Percona-Server/scripts/clustercheck.sh
@@ -1,52 +1,36 @@
 #!/bin/bash 
-# put this file to /usr/local/bin
 #
-# This script checks if a mysql server is healthy running on localhost. It will 
-# return: 
-# 
-# "HTTP/1.x 200 OK\r" (if mysql is running smoothly) 
-# 
-# - OR - 
-# 
-# "HTTP/1.x 500 Internal Server Error\r" (else) 
-# 
-# The purpose of this script is make haproxy capable of monitoring mysql properly 
-# 
-# Author: Unai Rodriguez 
-# 
-# It is recommended that a user with limited privileges is created to be used by 
-# this script. Something like this: 
-# 
-# mysql> GRANT PROCESS on mysql.* TO 'clustercheckuser'@'localhost' \ 
-#     -> IDENTIFIED BY 'clustercheckpassword!' WITH GRANT OPTION; 
-# mysql> flush privileges; 
+# Script to make a proxy (ie HAProxy) capable of monitoring Percona XtraDB Cluster nodes properly
+#
+# Author: Olaf van Zandwijk <olaf.vanzandwijk@nedap.com>
+# Documentation and download: https://github.com/olafz/percona-clustercheck
+#
+# Based on the original script from Unai Rodriguez 
+#
 
-MYSQL_HOST="localhost" 
-MYSQL_PORT="3306" 
 MYSQL_USERNAME="clustercheckuser" 
 MYSQL_PASSWORD="clustercheckpassword!" 
-TMP_FILE="/tmp/clustercheck.out" 
-ERR_FILE="/tmp/clustercheck.err" 
+ERR_FILE="/dev/null" 
+AVAILABLE_WHEN_DONOR=0
 
-# 
-# We perform a simple query that should return a few results :-p 
-# 
-
-WSSREP_STATUS=`mysql --host=$MYSQL_HOST --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD -e "show status like 'wsrep_local_state';" | awk '{if (NR!=1){print $2}}' 2>/dev/null` 
+#
+# Perform the query to check the wsrep_local_state
+#
+WSREP_STATUS=`mysql --user=${MYSQL_USERNAME} --password=${MYSQL_PASSWORD} -e "SHOW STATUS LIKE 'wsrep_local_state';" 2>${ERR_FILE} | awk '{if (NR!=1){print $2}}' 2>${ERR_FILE}` 
  
-if [ "$WSSREP_STATUS" == "4" ] 
+if [[ "${WSREP_STATUS}" == "4" ]] || [[ "${WSREP_STATUS}" == "2" && ${AVAILABLE_WHEN_DONOR} == 1 ]]
 then 
-    # mysql is fine, return http 200 
-    /bin/echo -e "HTTP/1.1 200 OK\r\n" 
-    /bin/echo -e "Content-Type: Content-Type: text/plain\r\n" 
-    /bin/echo -e "\r\n" 
-    /bin/echo -e "Node is running.\r\n" 
-    /bin/echo -e "\r\n" 
+    # Percona XtraDB Cluster node local state is 'Synced' => return HTTP 200
+    /bin/echo -en "HTTP/1.1 200 OK\r\n" 
+    /bin/echo -en "Content-Type: text/plain\r\n" 
+    /bin/echo -en "\r\n" 
+    /bin/echo -en "Percona XtraDB Cluster Node is synced.\r\n" 
+    /bin/echo -en "\r\n" 
 else 
-    # mysql is fine, return http 503 
-    /bin/echo -e "HTTP/1.1 503 Service Unavailable\r\n" 
-    /bin/echo -e "Content-Type: Content-Type: text/plain\r\n" 
-    /bin/echo -e "\r\n" 
-    /bin/echo -e "Node is *down*.\r\n" 
-    /bin/echo -e "\r\n" 
+    # Percona XtraDB Cluster node local state is not 'Synced' => return HTTP 503
+    /bin/echo -en "HTTP/1.1 503 Service Unavailable\r\n" 
+    /bin/echo -en "Content-Type: text/plain\r\n" 
+    /bin/echo -en "\r\n" 
+    /bin/echo -en "Percona XtraDB Cluster Node is not synced.\r\n" 
+    /bin/echo -en "\r\n" 
 fi 
