6ad3a7ad6cc676f5128bbbb17ebac4d904100121
Alexey Yurchenko <alexey.yurchenko@codership.com>
2012-09-02 23:59:45 +0700

    Fixes lp:1045040 - removed static SST method list and fixed wsrep_sst_method check call.

--- sql/wsrep_sst.cc
+++ sql/wsrep_sst.cc
@@ -41,31 +41,19 @@ static const char* sst_auth_real      = NULL;
 
 my_bool wsrep_sst_donor_rejects_queries = FALSE;
 
-static const char *sst_methods[] = {
-  "mysqldump",
-  "rsync",
-  "rsync_wan",
-  "xtrabackup",
-  NULL
-};
-
 bool wsrep_sst_method_check (sys_var *self, THD* thd, set_var* var)
 {
     char   buff[FN_REFLEN];
     String str(buff, sizeof(buff), system_charset_info), *res;
     const char* c_str = NULL;
 
-    if ((res = var->value->val_str(&str))) {
-      c_str = res->c_ptr();
-      int i = 0;
+    if ((res   = var->value->val_str(&str)) &&
+        (c_str = res->c_ptr()) &&
+        strlen(c_str) > 0)
+        return 0;
 
-      while (sst_methods[i] && strcasecmp(sst_methods[i], c_str)) i++;
-      if (!sst_methods[i]) {
-        my_error(ER_WRONG_VALUE_FOR_VAR, MYF(0), "wsrep_sst_method", c_str ? c_str : "NULL");
-        return 1;
-      }
-    }
-    return 0;
+    my_error(ER_WRONG_VALUE_FOR_VAR, MYF(0), "wsrep_sst_method", c_str ? c_str : "NULL");
+    return 1;
 }
 
 bool wsrep_sst_method_update (sys_var *self, THD* thd, enum_var_type type)
