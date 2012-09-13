84d08c30737e279463b266dbc321387f95ed049a
Seppo Jaakola <seppo.jaakola@codership.com>
2012-09-13 10:20:16 +0300

    References lp:1048492 - slave group commit cleanup

--- sql/wsrep_hton.cc
+++ sql/wsrep_hton.cc
@@ -115,17 +115,6 @@ static int wsrep_prepare(handlerton *hton, THD *thd, bool all)
       !thd_test_options(thd, OPTION_NOT_AUTOCOMMIT | OPTION_BEGIN)) &&
       (thd->variables.wsrep_on && !wsrep_trans_cache_is_empty(thd)))
   {
-#ifdef REMOVED
-    if (thd->system_thread== SYSTEM_THREAD_SLAVE_SQL &&
-        ++thd->wsrep_mysql_replicated < (int)wsrep_mysql_replication_bundle)
-    {
-      WSREP_DEBUG("skipping wsrep commit %d", thd->wsrep_mysql_replicated);
-    }
-    else
-    {
-      if (thd->system_thread== SYSTEM_THREAD_SLAVE_SQL)
-        thd->wsrep_mysql_replicated = 0;
-#endif
     switch (wsrep_run_wsrep_commit(thd, hton, all))
     {
     case WSREP_TRX_OK:
@@ -137,9 +126,6 @@ static int wsrep_prepare(handlerton *hton, THD *thd, bool all)
     case WSREP_TRX_ERROR:
       DBUG_RETURN(1);
     }
-#ifdef REMOVED
-    }
-#endif
   }
   DBUG_RETURN(0);
 }
