482bf73f6a3433856fd2fa59e61d03f941e8e2a1
Raghavendra D Prabhu <raghavendra.prabhu@percona.com>
2013-07-26 22:46:56 +0530

    Fix error with galera

--- build/build-binary-nobzr.sh
+++ build/build-binary-nobzr.sh
@@ -140,7 +140,9 @@ else
 fi 
 
 WSREP_REV="$(cd "$SOURCEDIR";test -r WSREP-REVISION && cat WSREP-REVISION || echo "$REVISION")"
-GALERA_REVISION="$(cd "$SOURCEDIR/percona-xtradb-cluster-galera"; test -r GALERA-REVISION && cat GALERA-REVISION || bzr revno)"
+if [[ $COPYGALERA -eq 0 ]];then 
+    GALERA_REVISION="$(cd "$SOURCEDIR/percona-xtradb-cluster-galera"; test -r GALERA-REVISION && cat GALERA-REVISION || bzr revno)"
+fi
 PRODUCT_FULL="$PRODUCT-$RELEASE_TAG$WSREP_VERSION.$REVISION.$(uname -s).$TARGET"
 COMMENT="Percona XtraDB Cluster (GPL) $MYSQL_VERSION-$RELEASE_TAG$WSREP_VERSION"
 COMMENT="$COMMENT, Revision $REVISION"
