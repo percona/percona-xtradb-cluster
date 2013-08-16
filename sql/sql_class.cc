111100a16ccae6e513881e3c0e9f7eb742ee53cb
Seppo Jaakola <seppo.jaakola@codership.com>
2013-08-16 15:42:50 +0300

    References: lp:1212247 - fix to support CREATE TABLE t LIKE <temporary table>

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -1073,7 +1073,8 @@ THD::THD()
   wsrep_consistency_check = NO_CONSISTENCY_CHECK;
   wsrep_status_vars       = 0;
   wsrep_mysql_replicated  = 0;
-
+  wsrep_TOI_pre_query     = NULL;
+  wsrep_TOI_pre_query_len = 0;
 #endif
   /* Call to init() below requires fully initialized Open_tables_state. */
   reset_open_tables_state(this);
@@ -1445,6 +1446,8 @@ void THD::init(void)
   wsrep_seqno_changed= false;
   wsrep_consistency_check = NO_CONSISTENCY_CHECK;
   wsrep_mysql_replicated  = 0;
+  wsrep_TOI_pre_query     = NULL;
+  wsrep_TOI_pre_query_len = 0;
 #endif
 
   if (variables.sql_log_bin)
--- sql/sql_class.h
+++ sql/sql_class.h
@@ -2218,6 +2218,9 @@ public:
                             wsrep_consistency_check;
   wsrep_stats_var*          wsrep_status_vars;
   int                       wsrep_mysql_replicated;
+  const char*               wsrep_TOI_pre_query; /* a query to apply before 
+						    the actual TOI query */
+  size_t                    wsrep_TOI_pre_query_len;
 #endif /* WITH_WSREP */
   /**
     Internal parser state.
