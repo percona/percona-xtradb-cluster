1ddaaed763cb6656abce077a3f4ad19ae8588491
Vladislav Klyachin <vladislav.klyachin@codership.com>
2013-09-03 15:24:04 +0400

    References lp:1049599 - expose desync functionality to the client

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -692,6 +692,7 @@ wsrep_aborting_thd_t wsrep_aborting_thd= NULL;
 mysql_mutex_t LOCK_wsrep_replaying;
 mysql_cond_t  COND_wsrep_replaying;
 mysql_mutex_t LOCK_wsrep_slave_threads;
+mysql_mutex_t LOCK_wsrep_desync;
 int wsrep_replaying= 0;
 static void wsrep_close_threads(THD* thd);
 #endif
@@ -1702,6 +1703,7 @@ static void clean_up_mutexes()
   (void) mysql_mutex_destroy(&LOCK_wsrep_replaying);
   (void) mysql_cond_destroy(&COND_wsrep_replaying);
   (void) mysql_mutex_destroy(&LOCK_wsrep_slave_threads);
+  (void) mysql_mutex_destroy(&LOCK_wsrep_desync);
 #endif
 }
 #endif /*EMBEDDED_LIBRARY*/
@@ -3749,6 +3751,8 @@ static int init_thread_environment()
   mysql_cond_init(key_COND_wsrep_replaying, &COND_wsrep_replaying, NULL);
   mysql_mutex_init(key_LOCK_wsrep_slave_threads, 
 		   &LOCK_wsrep_slave_threads, MY_MUTEX_INIT_FAST);
+  mysql_mutex_init(key_LOCK_wsrep_desync, 
+		   &LOCK_wsrep_desync, MY_MUTEX_INIT_FAST);
 #endif
   return 0;
 }
@@ -8589,7 +8593,7 @@ PSI_mutex_key key_BINLOG_LOCK_index, key_BINLOG_LOCK_prep_xids,
 PSI_mutex_key key_LOCK_wsrep_rollback, key_LOCK_wsrep_thd, 
   key_LOCK_wsrep_replaying, key_LOCK_wsrep_ready, key_LOCK_wsrep_sst, 
   key_LOCK_wsrep_sst_thread, key_LOCK_wsrep_sst_init, 
-  key_LOCK_wsrep_slave_threads;
+  key_LOCK_wsrep_slave_threads, key_LOCK_wsrep_desync;
 #endif
 PSI_mutex_key key_RELAYLOG_LOCK_index;
 PSI_mutex_key key_LOCK_thread_created;
@@ -8655,6 +8659,7 @@ static PSI_mutex_info all_server_mutexes[]=
   { &key_LOCK_wsrep_thd, "THD::LOCK_wsrep_thd", 0},
   { &key_LOCK_wsrep_replaying, "LOCK_wsrep_replaying", PSI_FLAG_GLOBAL},
   { &key_LOCK_wsrep_slave_threads, "LOCK_wsrep_slave_threads", PSI_FLAG_GLOBAL},
+  { &key_LOCK_wsrep_desync, "LOCK_wsrep_desync", PSI_FLAG_GLOBAL},
 #endif
   { &key_PARTITION_LOCK_auto_inc, "HA_DATA_PARTITION::LOCK_auto_inc", 0},
   { &key_LOCK_thread_created, "LOCK_thread_created", PSI_FLAG_GLOBAL }
