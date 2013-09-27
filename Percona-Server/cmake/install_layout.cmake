6f688a170166a08a78bb12599c5ffabe806dbc54
Raghavendra D Prabhu <raghavendra.prabhu@percona.com>
2013-09-27 15:29:36 +0530

    Fix issue with xinetd II

--- Percona-Server/cmake/install_layout.cmake
+++ Percona-Server/cmake/install_layout.cmake
@@ -171,6 +171,7 @@ SET(INSTALL_SUPPORTFILESDIR_RPM         "share/mysql")
 SET(INSTALL_MYSQLDATADIR_RPM            "/var/lib/mysql")
 SET(INSTALL_PLUGINTESTDIR_RPM           ${plugin_tests})
 
+SET(INSTALL_XINETDDIR_RPM               "/etc/xinetd.d")
 #
 # DEB layout
 #
@@ -197,6 +198,7 @@ SET(INSTALL_SUPPORTFILESDIR_DEB         "support-files")
 SET(INSTALL_MYSQLDATADIR_DEB            "data")
 SET(INSTALL_PLUGINTESTDIR_DEB           ${plugin_tests})
 
+SET(INSTALL_XINETDDIR_DEB               "/etc/xinetd.d")
 #
 # SVR4 layout
 #
@@ -223,6 +225,7 @@ SET(INSTALL_SUPPORTFILESDIR_SVR4        "support-files")
 SET(INSTALL_MYSQLDATADIR_SVR4           "/var/lib/mysql")
 SET(INSTALL_PLUGINTESTDIR_SVR4          ${plugin_tests})
 
+SET(INSTALL_XINETDDIR_SVR4              "/etc/xinetd.d")
 
 # Clear cached variables if install layout was changed
 IF(OLD_INSTALL_LAYOUT)
@@ -236,7 +239,7 @@ SET(OLD_INSTALL_LAYOUT ${INSTALL_LAYOUT} CACHE INTERNAL "")
 # will be defined  as ${INSTALL_BINDIR_STANDALONE} by default if STANDALONE
 # layout is chosen)
 FOREACH(var BIN SBIN LIB MYSQLSHARE SHARE PLUGIN INCLUDE SCRIPT DOC MAN
-  INFO MYSQLTEST SQLBENCH DOCREADME SUPPORTFILES MYSQLDATA PLUGINTEST)
+  INFO MYSQLTEST SQLBENCH DOCREADME SUPPORTFILES MYSQLDATA PLUGINTEST XINETD)
   SET(INSTALL_${var}DIR  ${INSTALL_${var}DIR_${INSTALL_LAYOUT}}
   CACHE STRING "${var} installation directory" ${FORCE})
   MARK_AS_ADVANCED(INSTALL_${var}DIR)
