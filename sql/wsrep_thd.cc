dc36d6dc77508edcbb224a947de17cd9fa51f9c8
Teemu Ollakka <teemu.ollakka@codership.com>
2013-10-25 12:21:05 +0300

    References lp:1243150 - transaction flags for BF threads, check thd->stmt_da->status() before calling my_ok() after replay

--- sql/wsrep_thd.cc
+++ sql/wsrep_thd.cc
@@ -93,8 +93,6 @@ static void wsrep_prepare_bf_thd(THD *thd, struct wsrep_thd_shadow* shadow)
   thd->net.vio= 0;
   thd->clear_error();
 
-  thd->variables.option_bits|= OPTION_NOT_AUTOCOMMIT;
-
   shadow->tx_isolation        = thd->variables.tx_isolation;
   thd->variables.tx_isolation = ISO_READ_COMMITTED;
   thd->tx_isolation           = ISO_READ_COMMITTED;
@@ -140,6 +138,11 @@ void wsrep_replay_transaction(THD *thd)
                   (long long)wsrep_thd_trx_seqno(thd));
       struct wsrep_thd_shadow shadow;
       wsrep_prepare_bf_thd(thd, &shadow);
+
+      /* From trans_begin() */
+      thd->variables.option_bits|= OPTION_BEGIN;
+      thd->server_status|= SERVER_STATUS_IN_TRANS;
+
       int rcode = wsrep->replay_trx(wsrep,
                                     &thd->wsrep_ws_handle,
                                     (void *)thd);
@@ -160,6 +163,14 @@ void wsrep_replay_transaction(THD *thd)
         if (thd->stmt_da->is_sent)
         {
           WSREP_WARN("replay ok, thd has reported status");
+        }
+        else if (thd->stmt_da->is_set())
+        {
+          if (thd->stmt_da->status() != Diagnostics_area::DA_OK)
+          {
+            WSREP_WARN("replay ok, thd has error status %d",
+                       thd->stmt_da->status());
+          }
         }
         else
         {
@@ -206,6 +217,10 @@ static void wsrep_replication_process(THD *thd)
 
   struct wsrep_thd_shadow shadow;
   wsrep_prepare_bf_thd(thd, &shadow);
+
+  /* From trans_begin() */
+  thd->variables.option_bits|= OPTION_BEGIN;
+  thd->server_status|= SERVER_STATUS_IN_TRANS;
 
   rcode = wsrep->recv(wsrep, (void *)thd);
   DBUG_PRINT("wsrep",("wsrep_repl returned: %d", rcode));
