dd86dc93d93bde2ced83b4dfc8436ca0903283c4
Teemu Ollakka <teemu.ollakka@codership.com>
2013-11-10 13:44:07 +0200

    References lp:1249753 - free thd->mem_root on appliers at post commit cleanup

--- sql/wsrep_applier.cc
+++ sql/wsrep_applier.cc
@@ -329,6 +329,7 @@ wsrep_cb_status_t wsrep_commit_cb(void*         const     ctx,
 
   wsrep_set_apply_format(thd, NULL);
   thd->mdl_context.release_transactional_locks();
+  free_root(thd->mem_root,MYF(MY_KEEP_PREALLOC));
   thd->tx_isolation= (enum_tx_isolation) thd->variables.tx_isolation;
 
   if (wsrep_slave_count_change < 0 && commit && WSREP_CB_SUCCESS == rcode)
