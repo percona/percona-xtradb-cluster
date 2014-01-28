24d20f30c5c4f2577114be109d87919cd19c3fe6
Raghavendra D Prabhu <raghavendra.prabhu@percona.com>
2014-01-28 20:57:56 +0530

    Add documentation to mysqlchk

--- Percona-Server/scripts/mysqlchk.sh
+++ Percona-Server/scripts/mysqlchk.sh
@@ -11,7 +11,14 @@ service mysqlchk
         user            = nobody 
         server          = /usr/bin/clustercheck
         log_on_failure  += USERID 
-        only_from       = 0.0.0.0/0 
+        only_from       = 0.0.0.0/0
+        #
+        # Passing arguments to clustercheck
+        # <user> <pass> <available_when_donor=0|1> <log_file> <available_when_readonly=0|1> <defaults_extra_file>"
+        # Recommended: server_args   = user pass 1 /var/log/log-file 0 /etc/my.cnf.local"
+        # Compatibility: server_args = user pass 1 /var/log/log-file 1 /etc/my.cnf.local"
+        # 55-to-56 upgrade: server_args = user pass 1 /var/log/log-file 0 /etc/my.cnf.extra"
+        #
         # recommended to put the IPs that need 
         # to connect exclusively (security purposes) 
         per_source      = UNLIMITED 
