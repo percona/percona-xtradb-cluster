9bac6953ec426a2f452b530ea86de09867f0fcae
Raghavendra D Prabhu <raghavendra.prabhu@percona.com>
2014-01-29 05:35:55 +0530

    Add #ifdef around values changed for Bug#1243228 in revno 678

--- Percona-Server/sql/sys_vars.cc
+++ Percona-Server/sql/sys_vars.cc
@@ -766,7 +766,11 @@ static Sys_var_enum Sys_binlog_format(
        "MIXED, the format switches to row-based and back implicitly per each "
        "query accessing an NDBCLUSTER table",
        SESSION_VAR(binlog_format), CMD_LINE(REQUIRED_ARG, OPT_BINLOG_FORMAT),
+#ifdef WITH_WSREP
        binlog_format_names, DEFAULT(BINLOG_FORMAT_ROW),
+#else 
+       binlog_format_names, DEFAULT(BINLOG_FORMAT_STMT),
+#endif
        NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(binlog_format_check),
        ON_UPDATE(fix_binlog_format_after_update));
 
