8da017a02759badc1d87bad0a3b64a5094e3d57b
Seppo Jaakola <seppo.jaakola@codership.com>
2014-01-31 00:36:26 +0200

    Refrences: lp:1272920 - fixed debug assert firing on varchar key changes

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -4503,7 +4503,7 @@ wsrep_innobase_mysql_sort(
 
 		tmp_length = charset->coll->strnxfrm(charset, str, str_length,
 						     tmp_str, str_length);
-		DBUG_ASSERT(tmp_length == str_length);
+		DBUG_ASSERT(tmp_length <= str_length);
  
 		break;
 	}
