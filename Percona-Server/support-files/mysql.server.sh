c471a2e858ee832325546a4ba47ad3758e9bc9da
Raghavendra D Prabhu <raghavendra.prabhu@percona.com>
2014-03-19 03:58:37 +0530

    Merge fix for Bug#1291024 from  5.5 tree

--- Percona-Server/support-files/mysql.server.sh
+++ Percona-Server/support-files/mysql.server.sh
@@ -406,6 +406,17 @@ case "$mode" in
     fi
     ;;
 
+  'restart-bootstrap')
+    # Stop the service and regardless of whether it was
+    # running or not, start it again.
+    if $0 stop  $other_args; then
+      $0 bootstrap-pxc $other_args
+    else
+      log_failure_msg "Failed to stop running server, so refusing to try to start."
+      exit 1
+    fi
+    ;;
+
   'reload'|'force-reload')
     if test -s "$mysqld_pid_file_path" ; then
       read mysqld_pid <  "$mysqld_pid_file_path"
@@ -429,7 +440,7 @@ case "$mode" in
     *)
       # usage
       basename=`basename "$0"`
-      echo "Usage: $basename {start|stop|restart|reload|force-reload|status|bootstrap-pxc}  [ MySQL (Percona XtraDB Cluster) options ]"
+      echo "Usage: $basename {start|stop|restart|restart-bootstrap|reload|force-reload|status|bootstrap-pxc}  [ MySQL (Percona XtraDB Cluster) options ]"
       exit 1
     ;;
 esac
