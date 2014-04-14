301ff18c3ca94d324ed6483fe3da6fa06d2694fd
Yan Zhang <dirtysalt1987@gmail.com>
2014-04-14 19:47:54 +0800

    References lp:1099783 fetch port info in an uniformed way. because sometimes netstat -nltp can not be able to print pid/progname in time, instead print -.

--- scripts/wsrep_sst_rsync.sh
+++ scripts/wsrep_sst_rsync.sh
@@ -52,24 +52,18 @@ check_pid_and_port()
     local rsync_pid=$2
     local rsync_port=$3
 
-    if [ "$OS" == "Darwin" -o "$OS" == "FreeBSD" ]; then
-        # no netstat --program(-p) option in Darwin and FreeBSD
-        local port_info=$(lsfo -i -Pn 2>/dev/null | grep "(LISTEN)" | \
-            grep ":$rsync_port")
-        local is_rsync=$(echo $port_info | \
-            grep -w '^rsync[[:space:]]\+'"$rsync_pid" 2>/dev/null)
-    else
-        local port_info=$(netstat -lntp 2>/dev/null | grep "LISTEN" | \
-            grep ":$rsync_port")
-        local is_rsync=$(echo $port_info | \
-            grep $rsync_pid/rsync 2>/dev/null)
-    fi
+    local port_info=$(lsof -i :$rsync_port -Pn 2>/dev/null | \
+        grep "(LISTEN)")
+    local is_rsync=$(echo $port_info | \
+        grep -w '^rsync[[:space:]]\+'"$rsync_pid" 2>/dev/null)
+
     if [ -n "$port_info" -a -z "$is_rsync" ]; then
         wsrep_log_error "rsync daemon port '$rsync_port' has been taken"
         exit 16 # EBUSY
     fi
     check_pid $pid_file && \
-        [ -n "$port_info" ] && [ -n "$is_rsync" ]
+        [ -n "$port_info" ] && [ -n "$is_rsync" ] && \
+        [ $(cat $pid_file) -eq $rsync_pid ]
 }
 
 MAGIC_FILE="$WSREP_SST_OPT_DATA/rsync_sst_complete"
