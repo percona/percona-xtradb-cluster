65512c1168b44e6f9549528ff1454e63a9f293cb
Raghavendra D Prabhu <raghavendra.prabhu@percona.com>
2014-05-22 19:33:32 +0530

    Merge changes for clustercheck from https://github.com/olafz/percona-clustercheck

--- Percona-Server/scripts/clustercheck.sh
+++ Percona-Server/scripts/clustercheck.sh
@@ -46,12 +46,17 @@ then
                     2>${ERR_FILE} | tail -1 2>>${ERR_FILE})
 
         if [[ "${READ_ONLY}" == "ON" ]];then 
+            # Percona XtraDB Cluster node local state is 'Synced', but it is in
+            # read-only mode. The variable AVAILABLE_WHEN_READONLY is set to 0.
+            # => return HTTP 503
+            # Shell return-code is 1
             echo -en "HTTP/1.1 503 Service Unavailable\r\n" 
             echo -en "Content-Type: text/plain\r\n" 
             echo -en "Connection: close\r\n" 
             echo -en "Content-Length: 43\r\n" 
             echo -en "\r\n" 
             echo -en "Percona XtraDB Cluster Node is read-only.\r\n" 
+            sleep 0.1
             exit 1
         fi
 
@@ -64,6 +69,7 @@ then
     echo -en "Content-Length: 40\r\n" 
     echo -en "\r\n" 
     echo -en "Percona XtraDB Cluster Node is synced.\r\n" 
+    sleep 0.1
     exit 0
 else 
     # Percona XtraDB Cluster node local state is not 'Synced' => return HTTP 503
@@ -74,5 +80,6 @@ else
     echo -en "Content-Length: 44\r\n" 
     echo -en "\r\n" 
     echo -en "Percona XtraDB Cluster Node is not synced.\r\n" 
+    sleep 0.1
     exit 1
 fi 
