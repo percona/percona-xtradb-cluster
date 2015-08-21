780f3e1bcb2aa150e2155b5f754840f5822e919e
Raghavendra D Prabhu <rprabhu@wnohang.net>
2015-08-21 18:44:14 +0530

    RM-115: Bump versions

--- cmake/wsrep.cmake
+++ cmake/wsrep.cmake
@@ -17,7 +17,7 @@
 # so WSREP_VERSION is produced regardless
 
 # Set the patch version
-SET(WSREP_PATCH_VERSION "11")
+SET(WSREP_PATCH_VERSION "12")
 INCLUDE(CheckFunctionExists)
 CHECK_FUNCTION_EXISTS(execvpe HAVE_EXECVPE)
 
