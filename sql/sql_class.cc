21012c09444535d9c657158b9907eae70deca705
Daniele Sciascia <daniele.sciascia@galeracluster.com>
2015-10-02 10:16:55 +0200

    refs codership/galera#308
    - Moves call wsrep_free_status() to THD::cleanup_after_query().
      Wsrep status variables were previously freed only on SHOW STATUS.
    - Removes valgrind suppression from mysql-test/valgrind.

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -2295,6 +2295,10 @@ void THD::cleanup_after_query()
   if (rli_slave)
     rli_slave->cleanup_after_query();
 #endif
+
+#ifdef WITH_WSREP
+  wsrep_free_status(this);
+#endif /* WITH_WSREP */
 }
 
 
