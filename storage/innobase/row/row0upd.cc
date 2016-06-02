d906f99b56cc49dcc802c9ccfb7ef80668905d49
Krunal Bauskar <krunal.bauskar@percona.com>
2016-06-02 16:44:54 +0530

    - PXC#592: Tried closing fk-reference-table that was never opened.
      Function "wsrep_row_upd_check_foreign_constraints" tried to mark
      fk-reference-table opened without ensuring it table is really opened.

--- storage/innobase/row/row0upd.cc
+++ storage/innobase/row/row0upd.cc
@@ -404,7 +404,7 @@ run_again:
 					dict_table_open_on_name(
 					  foreign->referenced_table_name_lookup,
 					  FALSE, FALSE, DICT_ERR_IGNORE_NONE);
-				opened = TRUE;
+				opened = (foreign->referenced_table) ? TRUE : FALSE;
 			}
 
 			if (foreign->referenced_table) {
@@ -427,7 +427,7 @@ run_again:
 					       ->n_foreign_key_checks_running);
 
 				if (opened == TRUE) {
-					dict_table_close(foreign->referenced_table, TRUE, FALSE);
+					dict_table_close(foreign->referenced_table, FALSE, FALSE);
 					opened = FALSE;
 				}
 			}
