ae81fea3927ddd77f90c16380224fc7dbce8302c
sjaakola <seppo.jaakola@iki.fi>
2016-08-11 22:16:13 +0300

    Refs: GCF-913 * merged fix for  MW-282

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -285,8 +285,27 @@ uint server_command_flags[COM_END+1];
 void init_update_queries(void)
 {
   /* Initialize the server command flags array. */
-#ifdef WITH_WSREP
   memset(server_command_flags, 0, sizeof(server_command_flags));
+  server_command_flags[COM_SLEEP]=               CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_INIT_DB]=             CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_QUERY]=               CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_FIELD_LIST]=          CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_REFRESH]=             CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_SHUTDOWN]=            CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_STATISTICS]=          CF_SKIP_QUESTIONS;
+  server_command_flags[COM_PROCESS_KILL]=        CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_PING]=                CF_SKIP_QUESTIONS;
+  server_command_flags[COM_STMT_PREPARE]=        CF_SKIP_QUESTIONS |
+                                                 CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_STMT_EXECUTE]=        CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_STMT_SEND_LONG_DATA]= CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_STMT_CLOSE]=          CF_SKIP_QUESTIONS |
+                                                 CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_STMT_RESET]=          CF_SKIP_QUESTIONS |
+                                                 CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_STMT_FETCH]=          CF_ALLOW_PROTOCOL_PLUGIN;
+  server_command_flags[COM_END]=                 CF_ALLOW_PROTOCOL_PLUGIN;
+#ifdef WITH_WSREP
   server_command_flags[COM_STATISTICS]=   CF_SKIP_QUESTIONS |
     CF_SKIP_WSREP_CHECK;
   server_command_flags[COM_PING]=         CF_SKIP_QUESTIONS |
@@ -316,26 +335,6 @@ void init_update_queries(void)
   */
   server_command_flags[COM_QUERY]= CF_SKIP_WSREP_CHECK;
   server_command_flags[COM_SET_OPTION]= CF_SKIP_WSREP_CHECK;
-#else
-  server_command_flags[COM_SLEEP]=               CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_INIT_DB]=             CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_QUERY]=               CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_FIELD_LIST]=          CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_REFRESH]=             CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_SHUTDOWN]=            CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_STATISTICS]=          CF_SKIP_QUESTIONS;
-  server_command_flags[COM_PROCESS_KILL]=        CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_PING]=                CF_SKIP_QUESTIONS;
-  server_command_flags[COM_STMT_PREPARE]=        CF_SKIP_QUESTIONS |
-                                                 CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_STMT_EXECUTE]=        CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_STMT_SEND_LONG_DATA]= CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_STMT_CLOSE]=          CF_SKIP_QUESTIONS |
-                                                 CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_STMT_RESET]=          CF_SKIP_QUESTIONS |
-                                                 CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_STMT_FETCH]=          CF_ALLOW_PROTOCOL_PLUGIN;
-  server_command_flags[COM_END]=                 CF_ALLOW_PROTOCOL_PLUGIN;
 #endif /* WITH_WSREP */
 
   /* Initialize the sql command flags array. */
