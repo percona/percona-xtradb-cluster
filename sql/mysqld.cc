2560fe0937dbd3e5e6f41ef8fcf84564af65e0c0
sjaakola <seppo.jaakola@iki.fi>
2016-08-11 22:22:10 +0300

    Refs: GCF-913 * merged fix for MW-279

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -7811,7 +7811,7 @@ static int mysql_init_variables(void)
   (void) strmake(mysql_home, tmpenv, sizeof(mysql_home)-1);
 #endif
 #ifdef WITH_WSREP
-  if (WSREP_ON && wsrep_init_vars())
+  if (wsrep_init_vars())
     return 1;
 #endif
   return 0;
