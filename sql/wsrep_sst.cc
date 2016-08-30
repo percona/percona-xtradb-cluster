ff11ed27d5f4676e6298625f37c4390ceb4ff71b
Krunal Bauskar <krunal.bauskar@percona.com>
2016-08-30 16:05:07 +0530

    - PXC#681: Avoid generating audit-log event for closing connection
      While closing connection audit-log event should not be generated.
      Fix for this was implemented in upstream but there are different
      classes for closing wsrep connection that failed to inherit this
      fix. Corrected it now.

--- sql/wsrep_sst.cc
+++ sql/wsrep_sst.cc
@@ -911,7 +911,7 @@ static void sst_reject_queries(my_bool close_conn)
 {
     wsrep_ready_set (FALSE); // this will be resotred when donor becomes synced
     WSREP_INFO("Rejecting client queries for the duration of SST.");
-    if (TRUE == close_conn) wsrep_close_client_connections(false);
+    if (TRUE == close_conn) wsrep_close_client_connections(false, false);
 }
 
 static int sst_donate_mysqldump (const char*         addr,
