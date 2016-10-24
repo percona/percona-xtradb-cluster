5d8e26c5791c184e9521d4fad8695076274ec36f
Krunal Bauskar <krunal.bauskar@percona.com>
2016-10-24 12:09:59 +0530

    - PXC#717: ALTER USER is not replicated
      CREATE/DROP USER are replicated but as listed here
      https://bugs.launchpad.net/codership-mysql/+bug/1376269 ALTER USER
      is not replicated.

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -6180,6 +6180,9 @@ create_sp_error:
     if (check_access(thd, UPDATE_ACL, "mysql", NULL, NULL, 1, 1) &&
         check_global_access(thd, CREATE_USER_ACL))
       break;
+#ifdef WITH_WSREP
+    WSREP_TO_ISOLATION_BEGIN(WSREP_MYSQL_DB, NULL, NULL)
+#endif /* WITH_WSREP */
     /* Conditionally writes to binlog */
     if (!(res= mysql_user_password_expire(thd, lex->users_list)))
       my_ok(thd);
