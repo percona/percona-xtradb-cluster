0a2c045e742ad750995a54e2afb7cf12e00d4fa0
Krunal Bauskar <krunal.bauskar@percona.com>
2018-10-01 20:36:56 +0530

    - 5.7.23 can cause crash on replay of transaction (if autocommit = off)
      - As per MySQL protocol, if autocommit = off then all statements
        following it will be treated as operating in multi-statement
        mode.
    
      - In PXC world, a local transaction could be replayed.
        Upstream recently added fix to avoid memory leak from empty
        transaction but a replaying transaction could be also an empty
        transaction so it needs to be skipped.
    
      - Condition to skip replaying transaction was corrected
        to include all use-cases of replaying transaction including
        when autocommit=off.
    
    (cherry picked from commit f5578f0b48100c199e0dc6300a4114345a14aa5c)

--- sql/wsrep_hton.cc
+++ sql/wsrep_hton.cc
@@ -232,6 +232,7 @@ void wsrep_post_commit(THD *thd, bool all) {
         rolls back to savepoint after first operation.
        */
       if (all && thd->wsrep_conflict_state != MUST_REPLAY &&
+          thd->wsrep_conflict_state != REPLAYING &&
           wsrep->post_rollback(wsrep, &thd->wsrep_ws_handle)) {
         WSREP_WARN("post_rollback fail: %llu %d", (long long)thd->thread_id(),
                    thd->get_stmt_da()->status());
