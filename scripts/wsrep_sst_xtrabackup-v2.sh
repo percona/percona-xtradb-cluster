291cd9f449e3ec67dc317c14bede2485a1f7e8d6
Daniele Sciascia <daniele.sciascia@galeracluster.com>
2020-12-22 10:13:16 +0100

    Fix test of '-k' option for timeout(1) in wsrep_sst_xtrabackup-v2
    wsrep_sst_xtrabackup-v2 script tests if timeout command has option
    `-k`. However this test fails, if `timeout --help` outputs to stderr.
    The fix consists of redirecting the output of `timeout --help` from
    stderr to stdout.
    
    Fixes codership/mysql-wsrep#379

--- scripts/wsrep_sst_xtrabackup-v2.sh
+++ scripts/wsrep_sst_xtrabackup-v2.sh
@@ -717,7 +717,7 @@ recv_joiner()
     set +e
 
     if [[ $tmt -gt 0 && -x `which timeout` ]];then 
-        if timeout --help | grep -q -- '-k';then 
+        if timeout --help 2>&1 | grep -q -- '-k';then
             ltcmd="timeout -k $(( tmt+10 )) $tmt $tcmd"
         else 
             ltcmd="timeout -s9 $tmt $tcmd"
