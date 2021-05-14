87046b67336211e8704d5e5caed204b24828cc0d
Venkatesh Prasad Venugopal <venkatesh.prasad@percona.com>
2021-05-14 19:06:24 +0530

    PXC-3619: Merge PS-8.0.23 (Update PXB version)
    ***
    Updated `wsrep_sst_xtrabackup-v2` script and set the required version to
    `8.0.23`.
    
    ***
    Updated `cmake/dev-helper.cmake` to download PXB-8.0.23.

--- scripts/wsrep_sst_xtrabackup-v2.sh
+++ scripts/wsrep_sst_xtrabackup-v2.sh
@@ -1640,7 +1640,7 @@ fi
 # 8.0.11  Transition-key fixes
 #
 
-XB_8x_REQUIRED_VERSION="8.0.22"
+XB_8x_REQUIRED_VERSION="8.0.23"
 
 if [[ ! -x $XTRABACKUP_80_PATH/bin/$XTRABACKUP_BIN ]]; then
     wsrep_log_error "******************* FATAL ERROR ********************** "
