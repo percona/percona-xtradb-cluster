f3b78465add4df5decc221f5bab5f740d717817b
Kamil Holubicki <kamil.holubicki@percona.com>
2022-03-04 21:56:13 +0100

    PXC-3789: PXC 8.0.27 refresh - Q4 2021 (MTR)
    https://jira.percona.com/browse/PXC-3789
    
    Fixed behavior related to PXC node acting as the semi-sync replication
    node.
    
    The problem was caused by changing defaults of
    replica_parallel_workers
    slave-parallel-type
    slave-preserve-commit-order
    (commit 7a74dceb300ed7d68a627ad50053bea9acdbb83e)

--- sql/rpl_replica_commit_order_manager.cc
+++ sql/rpl_replica_commit_order_manager.cc
@@ -499,9 +499,6 @@ bool Commit_order_manager::visit_lock_graph(
 }
 
 bool has_commit_order_manager(THD *thd) {
-#ifdef WITH_WSREP
-  if (WSREP(thd)) return false;
-#endif /* WITH_WSREP */
   return is_mts_worker(thd) &&
          thd->rli_slave->get_commit_order_manager() != nullptr;
 }
