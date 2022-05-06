6d052c1ea300542006570c386af898e38358c122
Kamil Holubicki <kamil.holubicki@percona.com>
2022-05-06 11:53:00 +0200

    PXC-3639: Incorrect usage of strncpy
    https://jira.percona.com/browse/PXC-3639
    
    Problem:
    strncpy usage in WSREP patch was not respecting buffer overflow in some
    cases. Other cases considered it, but code analysis was confusing as
    handling of it was not straightforward.
    
    Solution:
    Added buffer overflow checks where needed.
    Refactored code where confusing.

--- mysys_ssl/my_default.cc
+++ mysys_ssl/my_default.cc
@@ -128,7 +128,8 @@ static my_bool defaults_already_read= FALSE;
 /* The only purpose of this global array is to hold full name of my.cnf
  * which seems to be otherwise unavailable */
 char wsrep_defaults_file[FN_REFLEN + 10]={0,};
-char wsrep_defaults_group_suffix[FN_EXTLEN]={0,};
+static const char wsrep_defaults_group_suffix_empty= '\0';
+const char *wsrep_defaults_group_suffix= &wsrep_defaults_group_suffix_empty;
 #endif /* WITH_WREP */
 
 /* Which directories are searched for options (and in which order) */
@@ -569,7 +570,7 @@ int get_defaults_options(int argc, char **argv,
 #ifdef WITH_WSREP
       /* make sure we do this only once - for top-level file */
       if ('\0' == wsrep_defaults_group_suffix[0])
-        strncpy(wsrep_defaults_group_suffix, *group_suffix, sizeof(wsrep_defaults_group_suffix) - 1);
+        wsrep_defaults_group_suffix= *group_suffix;
 #endif /* WITH_WSREP */
 
       argc--;
@@ -945,8 +946,21 @@ static int search_default_file_with_ext(Process_option_func opt_handler,
   }
 #ifdef WITH_WSREP
   /* make sure we do this only once - for top-level file */
-  if ('\0' == wsrep_defaults_file[0])
+  if ('\0' == wsrep_defaults_file[0]) {
+    /* We are safe here to not truncate the filename, because of the check
+       done at the beginning of this function.
+       At this point 'name' is at most the sum of:
+       converted_dir_len + config_file: (FN_REFLEN-3)+1
+       dot: 1
+       ext: 4
+       terminating null: 1
+       => FN_REFLEN + 4
+       fn_format() which expands ~ to home directory path prevents overflowing
+       FN_REFLEN as well.
+        */
     strncpy(wsrep_defaults_file, name, sizeof(wsrep_defaults_file) - 1);
+    wsrep_defaults_file[sizeof(wsrep_defaults_file) - 1] = 0;
+  }
 #endif /* WITH_WSREP */
 
   while (mysql_file_getline(buff, sizeof(buff) - 1, fp, is_login_file))
