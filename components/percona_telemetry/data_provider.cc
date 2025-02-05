1cbab648b4d63c558ad84d46a4c45778af4199e6
Kamil Holubicki <kamil.holubicki@percona.com>
2025-02-05 21:29:10 +0100

    clang format fixed again 3

--- components/percona_telemetry/data_provider.cc
+++ components/percona_telemetry/data_provider.cc
@@ -91,7 +91,7 @@ class DbReplicationIdSolver {
     if (voter > id_voter_) {
       db_replication_id_ = id;
       id_voter_ = voter;
-      fprintf(stderr, "KH345");
+      fprintf(stderr, "KH3456");
     }
   }
 
