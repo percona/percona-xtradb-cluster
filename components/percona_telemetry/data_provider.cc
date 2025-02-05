9d18800fd64ba38553c4316a8ca6caddaa6f3f70
Kamil Holubicki <kamil.holubicki@percona.com>
2025-02-05 19:50:31 +0100

    clang format fixed again

--- components/percona_telemetry/data_provider.cc
+++ components/percona_telemetry/data_provider.cc
@@ -88,9 +88,9 @@ class DbReplicationIdSolver {
 #endif
 
   void vote(const std::string &id, Voter voter) {
-    if (voter > id_voter_){
-      db_replication_id_ =id;
-      id_voter_ =  voter;
+    if (voter > id_voter_) {
+      db_replication_id_ = id;
+      id_voter_ = voter;
     }
   }
 
