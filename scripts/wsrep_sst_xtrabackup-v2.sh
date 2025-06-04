0956abfe5429c1ac50f09b6e01f7867ae0e32590
Kamil Holubicki <kamil.holubicki@percona.com>
2025-06-04 12:32:35 +0200

    PXC-4603: Code refresh for - PXC 9.2.0
    Corrected PXB path in SST script

--- scripts/wsrep_sst_xtrabackup-v2.sh
+++ scripts/wsrep_sst_xtrabackup-v2.sh
@@ -144,7 +144,7 @@ DATA="${WSREP_SST_OPT_DATA}"
 XTRABACKUP_PATH_PREFIX="$(dirname $0)/pxc_extra/pxb-"
 
 # XB path compatible with the current version of PXC
-XTRABACKUP_THIS_VER_PATH="$(dirname $0)/pxc_extra/pxb-9.1"
+XTRABACKUP_THIS_VER_PATH="$(dirname $0)/pxc_extra/pxb-9.2"
 
 # XB path compatible with prev PXC version. It may be prev Innovative release or LTS
 # if current PXC version is 1st Innovative.
