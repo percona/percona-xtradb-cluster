2bf904cba337dccdead1f8bfe41117ede2b0a2da
Vadim Yalovets <vadim.yalovets@percona.com>
2026-09-03 18:46:22 +0300

    PXC-5223 Packaging tasks for release - PXC 9.7.1

--- build-ps/pxc_builder.sh
+++ build-ps/pxc_builder.sh
@@ -543,7 +543,7 @@ install_deps() {
         apt-get -y install doxygen doxygen-gui graphviz rsync libcurl4-openssl-dev
         apt-get -y install libcurl4-openssl-dev libre2-dev pkg-config libtirpc-dev libev-dev
         #apt-get -y install --download-only percona-xtrabackup-80=8.0.35-33-1.${DIST}
-        apt-get -y install --download-only percona-xtrabackup-84=8.4.0-5-1.${DIST}
+        apt-get -y install --download-only percona-xtrabackup-84=8.4.0-6-1.${DIST}
         apt-get -y install --download-only percona-xtrabackup-96=9.6.0-1-1.${DIST}
         apt-get -y install --download-only percona-xtrabackup-97=9.7.1~rc1-1.${DIST}
         if [ x"${DIST}" = xnoble -o x"${DIST}" = xtrixie ]; then
